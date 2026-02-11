eb1885ad0936063b6c1f715e7315a715ed98e277
alejandruxxug <alejandro.urrego@eia.edu.co>
2026-02-11 09:38:29 -0500

    Finished styles +html

---
+++ app.js
@@ -0,0 +1,4 @@
+form = document.getElementById('form');
+buttonForm = document.getElementById('generate');
+result = document.getElementById('result');
+restartButton = document.getElementById('restart');
